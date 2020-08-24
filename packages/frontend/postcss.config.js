c27e8578af7128cc04b8c95ade6567c7243cb6a3
LucasAlley <lucasalleydev@gmail.com>
2020-08-23 20:07:35 -0400

    postcss fixes

--- packages/frontend/postcss.config.js
+++ packages/frontend/postcss.config.js
@@ -1,4 +1,19 @@
-const tailwindcss = require("tailwindcss");
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  // Specify the paths to all of the template files in your project
+  content: ["./src/**/*.html", "./src/**/*.tsx", "./src/**/*.ts"],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: (content) => content.match(/[\w-/.:]+(?<!:)/g) || [],
+});
+
+const cssnano = require("cssnano")({
+  preset: "default",
+});
+
 module.exports = {
-  plugins: [tailwindcss("./tailwind.config.js"), require("autoprefixer")],
+  plugins: [
+    require("tailwindcss"),
+    require("autoprefixer"),
+    ...(process.env.NODE_ENV === "production" ? [purgecss, cssnano] : []),
+  ],
 };
